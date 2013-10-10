2e2039affd1ebb7115f865f6b0f66f384eb18cdb
Lloyd Cody <lloyd.cody@coatesdigital.com.au>
2013-10-10 23:07:57 +0000

    API URL changed to be relative path

--- mvc/View.js
+++ mvc/View.js
@@ -96,7 +96,7 @@ $JSKK.Class.create
 			
 			for (var template in this.templates)
 			{
-				var requestPath	=(this.$reflect('namespace').replace(/\./g,'/'))+'/html/'+this.templates[template];
+				var requestPath	=('/' + this.$reflect('namespace').replace(/\./g,'/'))+'/html/'+this.templates[template];
 				if (!this.getViewCache().exists(requestPath)
 				|| this.getViewCache().isFetching(requestPath))
 				{
